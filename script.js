9a396ec5e136da117f9854ced35e3ffae9917652
Mateusz <mateuszmielczarek93@gmail.comgit config --global color.ui truegit config --global user.email mateuszmielczarek93@gmail.com>
2020-04-10 11:34:44 +0200

    add parseint

--- script.js
+++ script.js
@@ -10,17 +10,29 @@ function MyFunction(){
          document.getElementById("SendButton").click();
          var Values = document.getElementById("InputBox").value
          document.getElementById("NameOut").innerHTML = Values;
+        
     }});
 
     BtnClick.onclick = function(){
         ModalHide.style.display = "block";
         var Values = document.getElementById("InputBox").value
         document.getElementById("NameOut").innerHTML = Values;
-    }
+       
+    };
 
     Close.onclick = function(){
         ModalHide.style.display = "none";
     }
 
-    
- };
+
+   ModalHide.addEventListener("keydown", function (event){
+       if(event.keyCode ===37)
+       //perseint
+    ModalHide.style.left; 
+    ModalHide.style.right; 
+    ModalHide.style.top;
+    ModalHide.style.bottom;
+
+  });
+
+};
